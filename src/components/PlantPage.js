95c66d004a626c80553c2370f9bd148e0e525b8e
Remberto Villar <rembertovillar@gmail.com>
2022-07-27 18:09:34 -0400

    commit tree 18adbe16ed8779a197e50448a4fd4e995ef27585

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
+
 function PlantPage() {
+
+  const [plants, SetPlants] = useState([])
+
+  useEffect( () => {
+    fetch('http://localhost:6001/plants')
+    .then(res => res.json())
+    .then(data => SetPlants(data))
+  }, [])
+
+  const addNewPlants = newPlant => {
+    SetPlants(plants => [newPlant, ...plants])
+  }
+
+  const [search, setSearch] = useState('')
+
+  const changeSearch = newSearch => {
+    setSearch(newSearch)
+  }
+
+  const filteredSearch = plants.filter((newPlant) => {
+    return newPlant.name.toLowerCase().includes(search.toLocaleLowerCase())
+  })
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlants={addNewPlants}/>
+      <Search changeSearch={changeSearch} />
+      <PlantList plants={filteredSearch} />
     </main>
   );
 }
